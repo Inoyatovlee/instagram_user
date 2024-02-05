e27be2d13917393a70f429b41709c8e35ead1b2c
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-05 23:06:58 +0500

    Tasbeh access

---
+++ lib/stores/tasbeh_controller.dart
@@ -0,0 +1,19 @@
+import 'package:get/get.dart';
+import 'package:uneversap_app/config/Imports.dart';
+
+class TasbehController extends GetxController {
+  String selected = "";
+  Map<String, dynamic> duolar = {
+    "Subhanalloh": {
+      "id": 1,
+      "title": "Subhanalloh",
+      "text": "Allohni poklab yod etaman"
+    },
+    "Alhamdulillah": {
+      "id": 1,
+      "title": "Alhamdulillah",
+      "text": "Allohga hamd bo'lsin"
+    },
+    "Allohu akbar": {"id": 1, "title": "Allohu akbar", "text": "Alloh buyuk"},
+  };
+}
