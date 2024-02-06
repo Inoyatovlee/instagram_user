96b5089cbe8b6cf4de700fd292f0395d79df10b7
inoyatovlee <inoyatovalijon02@gmail.com>
2024-02-07 00:01:43 +0500

    user icon, done: UniversalApp

--- lib/config/colors.dart
+++ lib/config/colors.dart
@@ -2,7 +2,7 @@ import 'package:uneversap_app/config/Imports.dart';
 
 class AppColors {
   static Color red = const Color.fromRGBO(142, 1, 1, 0.485);
-  static Color blue = const Color.fromRGBO(0, 74, 193, 0.49);
+  static Color blue = Color.fromARGB(204, 35, 114, 240);
   static Color yellow = const Color.fromRGBO(155, 145, 1, 0.486);
   static Color pink = const Color.fromARGB(99, 101, 1, 121);
   static Color violet = const Color.fromRGBO(121, 16, 1, 155);
